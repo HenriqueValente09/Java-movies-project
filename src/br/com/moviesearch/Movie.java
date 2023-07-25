f3c15176fc39d925f11474f1e8519d4bb065219b
HenriqueValente09 <valentedev09@gmail.com>
2023-07-25 13:18:27 -0300

    Now, Movie and Series extend Title

--- src/br/com/moviesearch/Movie.java
+++ src/br/com/moviesearch/Movie.java
@@ -1,39 +1,13 @@
 package br.com.moviesearch;
 
-public class Movie {
-    private String name;
-    private int releaseDate;
-    private boolean included;
-    private double rating;
-    private int reviewsAmount;
-    private int length;
+public class Movie extends Title {
+    private String director;
 
-    public void setName(String name) {
-        this.name = name;
+    public String getDirector() {
+        return director;
     }
 
-    public void setReleaseDate(int releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public void setIncluded(boolean included) {
-        this.included = included;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
-    }
-
-    public void data() {
-        System.out.println("Name: " + name);
-        System.out.println("Release Date: " + releaseDate);
-        System.out.println("Length: " + length);
-        System.out.println("Rating: " + String.format("%.2f", rating/reviewsAmount));
-        System.out.println("Reviews: " + reviewsAmount);
-    }
-
-    public void review(double userReview) {
-        rating += userReview;
-        reviewsAmount++;
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
