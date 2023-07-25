96478a013275bb6842457b8ae3d76e7717cb522c
HenriqueValente09 <valentedev09@gmail.com>
2023-07-25 13:18:27 -0300

    feat: Now Movies and Series extends Titles

---
+++ src/br/com/moviesearch/Show.java
@@ -0,0 +1,4 @@
+package br.com.moviesearch;
+
+public class Show extends Title{
+}
--- src/br/com/moviesearch/Title.java
+++ src/br/com/moviesearch/Title.java
@@ -1,4 +1,39 @@
 package br.com.moviesearch;
 
 public class Title {
+    private String name;
+    private int releaseDate;
+    private boolean included;
+    private double rating;
+    private int reviewsAmount;
+    private int length;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public void setIncluded(boolean included) {
+        this.included = included;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void data() {
+        System.out.println("Name: " + name);
+        System.out.println("Release Date: " + releaseDate);
+        System.out.println("Length: " + length);
+        System.out.println("Rating: " + String.format("%.2f", rating/reviewsAmount));
+        System.out.println("Reviews: " + reviewsAmount);
+    }
+
+    public void review(double userReview) {
+        rating += userReview;
+        reviewsAmount++;
+    }
 }
