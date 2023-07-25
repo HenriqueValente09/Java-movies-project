d095fb876a387a41276d490c75c88c6bee351473
HenriqueValente09 <valentedev09@gmail.com>
2023-07-25 13:53:36 -0300

    chore: Show class getters and setters

--- src/br/com/moviesearch/Show.java
+++ src/br/com/moviesearch/Show.java
@@ -4,6 +4,37 @@ public class Show extends Title{
     private int seasons;
     private int episodes;
     private int episodeDuration;
-
     private boolean active;
+
+    public int getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(int seasons) {
+        this.seasons = seasons;
+    }
+
+    public int getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
+    }
+
+    public int getEpisodeDuration() {
+        return episodeDuration;
+    }
+
+    public void setEpisodeDuration(int episodeDuration) {
+        this.episodeDuration = episodeDuration;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
