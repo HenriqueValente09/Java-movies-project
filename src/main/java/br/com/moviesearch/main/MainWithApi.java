d44f95368476d07898697f62b243d5197e33f3d4
HenriqueValente09 <valentedev09@gmail.com>
2023-08-30 19:12:32 -0300

    feat: Writing in files

--- src/main/java/br/com/moviesearch/main/MainWithApi.java
+++ src/main/java/br/com/moviesearch/main/MainWithApi.java
@@ -6,6 +6,10 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -13,10 +17,11 @@ import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class MainWithApi {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         String movie = scanner.nextLine();
         movie = movie.replace(" ", "-");
+        File file = new File("C:\\Users\\henri\\IdeaProjects\\movies\\movies.txt");
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -33,10 +38,15 @@ public class MainWithApi {
         } else {
             try {
                 Title title = new Title(omdbTitle);
+                Writer writer = new FileWriter(file);
+                writer.append(response);
+                writer.close();
                 title.getData();
             } catch (NumberFormatException e) {
                 System.out.println("Ocorreu um erro:");
                 System.out.println(e.getMessage());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
     }
